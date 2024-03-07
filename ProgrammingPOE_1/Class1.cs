04624a91a33c994ffe901054e6e28040ca649179
Snoxx <ST10253994@vcconnect.edu.za>
2024-03-07 14:37:25 +0200

    Commit_V5, added comments

--- ProgrammingPOE_1/Class1.cs
+++ ProgrammingPOE_1/Class1.cs
@@ -21,10 +21,14 @@ class Methods
         string ingrediantName, measure;
 
         Console.WriteLine("Please enter the amount of ingrediants: ");
-        ingrediantC = Convert.ToInt32(Console.ReadLine());
+        ingrediantC = Convert.ToInt32(Console.ReadLine()); // this value will be used for the forloop 
 
         for (int i = 0; i < ingrediantC; i++)
         {
+
+            /*
+             * the next 3 inputs will be stored in the arraylists ingrediants, quantity and measurements
+             */
 
             Console.WriteLine("Please enter your ingrediant name: ");
             ingrediantName = Console.ReadLine();
@@ -53,9 +57,9 @@ class Methods
         int stepA;
 
         Console.WriteLine("Please enter the amount of steps: ");
-        stepA = Convert.ToInt32(Console.ReadLine());
+        stepA = Convert.ToInt32(Console.ReadLine()); // this value will be used to determine the amount of times we go through the forloop
 
-        for (int i = 0; i < stepA; i++)
+        for (int i = 0; i < stepA; i++) // forloop is used to output and gain a input for as many steps as is required
         {
             Console.WriteLine("Please enter step " + (i + 1));
             step = Console.ReadLine();
@@ -69,14 +73,14 @@ class Methods
     public void PrintRecipe(List<string> steps, List<string> ingrediants, List<int> quantity, List<string> measurements)
     {
         Console.WriteLine("Recipe:\n\nIngrediants:");
-        for (int i = 0; i < ingrediants.Count; i++)
+        for (int i = 0; i < ingrediants.Count; i++) // forloop is used to print out all values saved in the arraylists ingrediants, quantity and measurements
         {
             Console.WriteLine(ingrediants[i]);
             Console.WriteLine(quantity[i] + measurements[i]);
         }
 
         Console.WriteLine("\nSteps: ");
-        for (int i = 0; i < steps.Count; i++)
+        for (int i = 0; i < steps.Count; i++) // forloop is used to print out all values stored in the arraylist steps
         {
             Console.WriteLine("Step " + (i + 1) + ":" + steps[i]);
         }
@@ -93,12 +97,12 @@ class Methods
         Console.WriteLine("How many people will there be?");
         factor = Convert.ToInt32(Console.ReadLine());
 
-        for (int i = 0; i < quantity.Count; i++)
+        for (int i = 0; i < quantity.Count; i++) // forloop is used to scale for the amount of people by multiplying the quantity at index i with the factor
         {
             amount = quantity[i] * factor;
             quantity[i] = amount;
         }
-        sum = sum * factor;
+        sum = sum * factor; // sum will be used to reset the scale for 1 portion later on
     }
 
     /*
@@ -109,7 +113,7 @@ class Methods
     {
         int amount;
 
-        for (int i = 0; i < quantity.Count; i++)
+        for (int i = 0; i < quantity.Count; i++) // is made to reset the quantity to 1 portion by deviding with the product of sum and factor
         {
             amount = quantity[i] / sum;
             quantity[i] = amount;
